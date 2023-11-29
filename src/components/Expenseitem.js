67c763677c35675826d226987618a8c0de3efcb3
HumaRukshan25 <1hk19cs061@hkbk.edu.in>
2023-11-29 13:16:22 +0530

    started with adding expense

---
+++ src/components/Expenseitem.js
@@ -0,0 +1,26 @@
+
+import React from 'react';
+
+function Expenseitem() {
+  return (
+    <div>
+      <div>March 28th, 2021</div>
+      <div>
+        <h2>Car Insurance</h2>
+        <div>$294.67</div>
+      </div>
+
+      {/* Expenses */}
+      <div>
+        <h3>Expense Items</h3>
+        <ul>
+          <li>Food Rs 10</li>
+          <li>Petrol Rs 100</li>
+          <li>Movies Rs 200</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default Expenseitem;
