78812702904dea1bf04c2dc1632fabe84359589d
HumaRukshan25 <1hk19cs061@hkbk.edu.in>
2023-12-01 18:49:16 +0530

    Delete Expense by dom manipualtion

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,23 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
+  const [isDeleted, setIsDeleted] = useState(false);
+
   const clickHandler = () => {
     console.log('Clicked!!!!');
   };
 
+  const deleteExpenseHandler = () => {
+    // Perform any additional cleanup or action before deletion if needed
+    // For now, just mark it as deleted
+    setIsDeleted(true);
+  };
+
   return (
-    <Card className='expense-item'>
+    <Card className={`expense-item ${isDeleted ? 'deleted' : ''}`}>
       <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
         <h2>{props.title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
       </div>
-      <div>
-        <button onClick={clickHandler}>Click Me</button>
-      </div>
+      {!isDeleted && (
+        <div>
+          <button onClick={clickHandler}>Click Me</button>
+          <button onClick={deleteExpenseHandler}>Delete Expense</button>
+        </div>
+      )}
     </Card>
   );
 }
